de189d710819a07c1b558c7e97a25fefc6ffbfc5
ghb130 <graehmeblair2019@u.northwestern.edu>
2018-04-29 15:21:58 -0500

    Completed translation functions

--- vhdl/IK/ik_pack.vhd
+++ vhdl/IK/ik_pack.vhd
@@ -12,9 +12,9 @@ package ik_pack is
   function slv_to_mat_4 (slv : std_logic_vector(511 downto 0))
                         return mat_4;
   function mat_3_to_slv (mat : mat_3)
-                        return std_logic_vector(287 downto 0);
+                       return std_logic_vector;
   function mat_4_to_slv (mat : mat_4)
-                        return std_logic_vector(511 downto 0);
+                       return std_logic_vector;
 end package ik_pack;
 
 package body ik_pack is
@@ -33,4 +33,66 @@ begin
   MAT(2)(2) := slv(31 downto 0);
   return MAT;
 end slv_to_mat_3;
+
+function slv_to_mat_4 (slv : std_logic_vector(511 downto 0))
+                      return mat_4 is
+  variable MAT : mat_4;
+begin
+  MAT(0)(0) := slv(511 downto 480);
+  MAT(0)(1) := slv(479 downto 448);
+  MAT(0)(2) := slv(447 downto 416);
+  MAT(0)(3) := slv(415 downto 384);
+  MAT(1)(0) := slv(383 downto 352);
+  MAT(1)(1) := slv(351 downto 320);
+  MAT(1)(2) := slv(319 downto 288);
+  MAT(1)(3) := slv(287 downto 256);
+  MAT(2)(0) := slv(255 downto 224);
+  MAT(2)(1) := slv(223 downto 192);
+  MAT(2)(2) := slv(191 downto 160);
+  MAT(2)(3) := slv(159 downto 128);
+  MAT(3)(0) := slv(127 downto 96);
+  MAT(3)(1) := slv(95 downto 64);
+  MAT(3)(2) := slv(63 downto 32);
+  MAT(3)(3) := slv(31 downto 0);
+  return MAT;
+end slv_to_mat_4;
+
+function mat_3_to_slv (mat : mat_3)
+                     return std_logic_vector is
+  variable slv : std_logic_vector(287 downto 0);
+begin
+  slv(287 downto 256) := mat(0)(0);
+  slv(255 downto 224) := mat(0)(1);
+  slv(223 downto 192) := mat(0)(2);
+  slv(191 downto 160) := mat(1)(0);
+  slv(159 downto 128) := mat(1)(1);
+  slv(127 downto 96) := mat(1)(2);
+  slv(95 downto 64) := mat(2)(0);
+  slv(63 downto 32) := mat(2)(1);
+  slv(31 downto 0) := mat(2)(2);
+  return slv;
+end mat_3_to_slv;
+
+function mat_4_to_slv (mat : mat_4)
+                     return std_logic_vector is
+  variable slv : std_logic_vector(511 downto 0);
+begin
+  slv(511 downto 480) := mat(0)(0);
+  slv(479 downto 448) := mat(0)(1);
+  slv(447 downto 416) := mat(0)(2);
+  slv(415 downto 384) := mat(0)(3);
+  slv(383 downto 352) := mat(1)(0);
+  slv(351 downto 320) := mat(1)(1);
+  slv(319 downto 288) := mat(1)(2);
+  slv(287 downto 256) := mat(1)(3);
+  slv(255 downto 224) := mat(2)(0);
+  slv(223 downto 192) := mat(2)(1);
+  slv(191 downto 160) := mat(2)(2);
+  slv(159 downto 128) := mat(2)(3);
+  slv(127 downto 96) := mat(3)(0);
+  slv(95 downto 64) := mat(3)(1);
+  slv(63 downto 32) := mat(3)(2);
+  slv(31 downto 0) := mat(3)(3);
+  return slv;
+end mat_4_to_slv;
 end package body ik_pack;
