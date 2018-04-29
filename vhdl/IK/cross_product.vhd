165613211bf30dbc0fdc8cc69eaa84437e06aa0d
drewhasse <drew091996@gmail.com>
2018-04-29 14:35:12 -0500

    package and stuff

---
+++ vhdl/IK/cross_product.vhd
@@ -0,0 +1,9 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use work.ik_pack.all;
+
+entity cross_product is
+  port (
+  clock
+  );
+end entity;
---
+++ vhdl/IK/ik_pack.vhd
@@ -0,0 +1,7 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_numeric.all;
+
+package ik_pack is
+  type vec_3 is array(0 to 2) of std_logic_vector(31 downto 0);
+end package;
