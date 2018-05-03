8b251313e6b166474c5bb34200a270491946aa0b
ghb130 <graehmeblair2019@u.northwestern.edu>
2018-05-03 16:49:16 -0500

    finished combinational fk

--- vhdl/FK/fk_comb.vhd
+++ vhdl/FK/fk_comb.vhd
@@ -19,7 +19,7 @@ entity fk_comb is
 end entity;
 
 architecture behavioral of fk_comb is
-  signal vec_r0, vec_t0, vec_r1, vec_t1, vec_r2, vec_t2 : std_logic_vector(95 downto 0);
+  signal vec_origin, vec_t2xo, vec_r2xt2, vec_t1xr2, vec_r1xt1, vec_t0xr1, vec_r0xt0 : std_logic_vector(95 downto 0);
   signal ex_i, ex_i_c, ey_i, ey_i_c : std_logic_vector(31 downto 0);
   signal sin_a0, cos_a0, sin_a0, cos_a0, sin_a0, cos_a0 : std_logic_vector(31 downto 0);
   signal mat_r0, mat_t0, mat_r1, mat_t1, mat_r2, mat_t2 : mat_3;
@@ -34,6 +34,8 @@ begin
         ey_i <= ey_i_c;
       end if;
   end process clocked;
+--- Init signals
+vec_origin <= (16 => '1', others => '0');
 --- Calculate sin and cos of input angles
   sincos_a0 : sincos_pipelined
   generic map (
@@ -77,7 +79,7 @@ begin
     Cos          => cos_a2
   );
 --- End sin/cos calculation
-
+--- Begin matrix formation
 mat_r0(0)(0) <= cos_a0;
 mat_r0(0)(1) <= std_logic_vector((NOT signed(sin_a0))+1);
 mat_r0(0)(2) <= (others => '0');
@@ -137,5 +139,48 @@ mat_t2(1)(2) <= (others => '0');
 mat_t2(2)(0) <= (others => '0');
 mat_t2(2)(1) <= (others => '0');
 mat_t2(2)(2) <= (16 => '1', others => '0');
+--- End matrix formation
+--- Begin matrix vector multiplication
+mat_3x1_t2xo : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_t2);
+  vec_3_r_in => vec_origin;
+  vec_3_out => vec_t2xo;
+);
+mat_3x1_r2xt2 : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_r2);
+  vec_3_r_in => vec_t2xo;
+  vec_3_out => vec_r2xt2;
+);
+mat_3x1_t1xr2 : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_t1);
+  vec_3_r_in => vec_r2xt2;
+  vec_3_out => vec_t1xr2;
+);
+mat_3x1_r1xt1 : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_r1);
+  vec_3_r_in => vec_t1xr2;
+  vec_3_out => vec_r1xt1;
+);
+mat_3x1_t0xr1 : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_t0);
+  vec_3_r_in => vec_r1xt1;
+  vec_3_out => vec_t0xr1;
+);
+mat_3x1_r0xt0 : mat_3x1
+port map (
+  mat_3_l_in => mat_3_to_slv(mat_r0);
+  vec_3_r_in => vec_t0xr1;
+  vec_3_out => vec_r0xt0;
+);
 
+ex_i_c <= vec_r0xt0(95 downto 64);
+ey_i_c <= vec_r0xt0(63 downto 32);
+ex <= ex_i;
+ey <= ey_i;
+--- End matrix vector multiplication
 end architecture;
